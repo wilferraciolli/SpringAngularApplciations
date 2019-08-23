9a4c12ddeab7036b20ea7f733848c7c876438133
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-08-23 20:05:29 +0100

    Created application with application.yml file created a profile top run dev or qa Autowired application config based on profile set

---
+++ adminDashBoard/src/main/java/com/wiltech/admin/dashboard/controller/RegionInfoConfiguration.java
@@ -0,0 +1,23 @@
+package com.wiltech.admin.dashboard.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+@ConfigurationProperties("regioninfo")
+public class RegionInfoConfiguration {
+
+    private String region;
+    private Integer version;
+
+    public RegionInfoConfiguration() {
+    }
+
+    public RegionInfoConfiguration(final String region, final Integer version) {
+        this.region = region;
+        this.version = version;
+    }
+}
