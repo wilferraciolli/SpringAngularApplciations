3f6ced6d2033fae6050e24e58f78a54f418bded9
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-09-27 17:54:55 +0100

    Added backend

---
+++ Spring Angular Keycloak/keycloak-tutorial-api/src/main/java/com/wiltech/springangularkeycloak/SpringAngularKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.wiltech.springangularkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularKeycloakApplication.class, args);
+	}
+
+}
---
+++ Spring Angular Keycloak/keycloak-tutorial-api/src/test/java/com/wiltech/springangularkeycloak/SpringAngularKeycloakApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wiltech.springangularkeycloak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAngularKeycloakApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
